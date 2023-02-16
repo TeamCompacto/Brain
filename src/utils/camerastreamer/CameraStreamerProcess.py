c75923409966e355e221c01ba33af4006a37c2bc
Péter Ákos <89993440+Peter-Akos@users.noreply.github.com>
2023-02-16 23:06:42 +0200

    Update CameraStreamerProcess.py

--- src/utils/camerastreamer/CameraStreamerProcess.py
+++ src/utils/camerastreamer/CameraStreamerProcess.py
@@ -75,7 +75,7 @@ class CameraStreamerProcess(WorkerProcess):
     def _init_socket(self):
         """Initialize the socket client. 
         """
-        self.serverIp   =  '192.168.43.17' # PC ip
+        self.serverIp   =  '192.168.43.231' # PC ip
         self.port       =  6666            # com port
 
         self.client_socket = socket.socket()
