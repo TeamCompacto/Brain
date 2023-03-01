0e5f33e45b2ee1fb4baaa8137259f06ae03d29a4
Péter Ákos <89993440+Peter-Akos@users.noreply.github.com>
2023-03-01 16:47:50 +0200

    Computer Vision Process

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ import sys
 
 from src.behaviour.decision_making.decision_making import DecisionMakingProcess
 from src.perception.object_classi.object_classi import ObjectDetectionProcess
+from src.perception import ComputerVisionProcess
 sys.path.append('.')
 
 import time
