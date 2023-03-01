35e29268d71b18dac459ce38abd5658fd23ecd07
Péter Ákos <89993440+Peter-Akos@users.noreply.github.com>
2023-03-01 17:57:21 +0200

    asd

--- src/perception/object_classi/ComputerVisionProcess.py
+++ src/perception/object_classi/ComputerVisionProcess.py
@@ -82,7 +82,7 @@ class ComputerVisionProcess(WorkerProcess):
 
             print(type(image))
             print(image.shape)
-            
+
             img0 = image
 
             img = letterbox(img0, new_shape = (self.img_size, self.img_size))[0]
