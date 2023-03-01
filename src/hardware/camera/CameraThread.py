8ccb10f98b43e0162792388d2a9a75a57a2a2379
Péter Ákos <89993440+Peter-Akos@users.noreply.github.com>
2023-03-01 11:57:11 +0200

    Update CameraThread.py

--- src/hardware/camera/CameraThread.py
+++ src/hardware/camera/CameraThread.py
@@ -124,7 +124,7 @@ class CameraThread(ThreadWithStop):
         from picamera2.outputs import FileOutput
 
         self.camera = Picamera2()
-        capture_config = self.camera.create_still_configuration({"size": (1640,1232)})
+        capture_config = self.camera.create_still_configuration(raw={"size": (1640,1232)})
         self.camera.configure(capture_config)
 
 
