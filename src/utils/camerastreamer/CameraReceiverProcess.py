81e3fe90ffe9e8d28598a6dea97cf817d2466de5
Péter Ákos <89993440+Peter-Akos@users.noreply.github.com>
2023-03-03 22:37:51 +0200

    Update CameraReceiverProcess.py

--- src/utils/camerastreamer/CameraReceiverProcess.py
+++ src/utils/camerastreamer/CameraReceiverProcess.py
@@ -61,7 +61,7 @@ class CameraReceiverProcess(WorkerProcess):
 
         
 
-        self.imgSize    = (1232,1640,3)
+        self.imgSize    = (320,320,3)
     # ===================================== RUN ==========================================
     def run(self):
         """Apply the initializers and start the threads. 
