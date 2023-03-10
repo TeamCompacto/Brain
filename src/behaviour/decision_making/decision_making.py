762d01d397498178e2fdce0a41bea868a2f10601
Péter Ákos <peterakos19@gmail.com>
2023-03-10 23:06:15 +0200

    Update decision_making.py

--- src/behaviour/decision_making/decision_making.py
+++ src/behaviour/decision_making/decision_making.py
@@ -2,6 +2,7 @@ import socket
 from threading import Thread
 from src.communication.enums.obstacles import Obstacles
 from src.templates.workerprocess import WorkerProcess
+import time
 
 class DecisionMakingProcess(WorkerProcess):
     
@@ -18,7 +19,9 @@ class DecisionMakingProcess(WorkerProcess):
     def _decision_making_thread(self, inPs, outPs):
         print("Decision Making Started")
         outPs.send({'action': '3', 'brake (steerAngle)': 0.0} )
-        # outPs.send({'action': '1', 'speed': 0.12} )
+        outPs.send({'action': '1', 'speed': 0.12} )
+        time.sleep(0.2)
+        outPs.send({'action': '1', 'speed': 0.09} )
         try:
             while True:
                 [deviation] = inPs[0].recv()
