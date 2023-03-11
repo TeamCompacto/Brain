b88ef35ada27a220babbe9b86100b797b84db1bf
Péter Ákos <peterakos19@gmail.com>
2023-03-11 20:28:09 +0200

    Update LaneKeepingAlgorithm.py

--- LaneKeepingAlgorithm.py
+++ LaneKeepingAlgorithm.py
@@ -38,6 +38,15 @@ import math
 # throttle = GPIO.PWM(throttlePin,1000)
 # throttle.stop()
 
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIOs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+         12, 13, 16, 17, 18, 19, 20, 21,
+         22, 23, 24, 25, 26, 27]
+
+for gpio in GPIOs:
+    print("GPIO no " + str(gpio) + ": " + str(GPIO.input(gpio)))
+
 
 
 def detect_edges(frame):
