3e929ffce07d4b5583723a785ef866477d7a8306
Péter Ákos <peterakos19@gmail.com>
2023-03-12 21:07:31 +0200

    Update new_main.py

--- new_main.py
+++ new_main.py
@@ -300,11 +300,12 @@ def park_parallel(pipe):
         pipe.send({'action': '1', 'speed': parking_speed_reverse})
         pipe.send({'action': '2', 'steerAngle': 0.0})
         time.sleep(0.3)
+        pipe.send({'action': '3', 'brake (steerAngle)': 0.0} )
 
 
 
         pipe.send({'action': '1', 'speed': parking_speed})
-        pipe.send({'action': '2', 'steerAngle': -20.0})
+        pipe.send({'action': '2', 'steerAngle': -17.0})
         time.sleep(1.5)
 
         
